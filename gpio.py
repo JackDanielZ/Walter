9eea0deab238ca18af86706c7304a6f2ab59e540
Daniel Zaoui <daniel@eyoman.com>
2024-02-14 09:50:55 +0200

    Update GPIO functions

--- gpio.py
+++ gpio.py
@@ -4,17 +4,16 @@ import stat
 GPIO_ROOT = '/sys/class/gpio'
 GPIO_EXPORT = os.path.join(GPIO_ROOT, 'export')
 GPIO_UNEXPORT = os.path.join(GPIO_ROOT, 'unexport')
-FMODE = 'w'
 IN, OUT = 'in', 'out'
 LOW, HIGH = 0, 1
 
 def GPIOExport(pin):
-    with open(GPIO_EXPORT, FMODE) as f:
+    with open(GPIO_EXPORT, 'w') as f:
         f.write(str(pin))
 #        f.flush()
 
 def GPIOUnexport(pin):
-    with open(GPIO_UNEXPORT, FMODE) as f:
+    with open(GPIO_UNEXPORT, 'w') as f:
         f.write(str(pin))
 #        f.flush()
 
@@ -26,12 +25,12 @@ def GPIOExists(pin):
     return os.path.isfile(GPIO_ROOT+"/gpio"+str(pin)+"/direction")
 
 def GPIODirectionSet(pin, dir):
-    with open(GPIO_ROOT+"/gpio"+str(pin)+"/direction", FMODE) as f:
+    with open(GPIO_ROOT+"/gpio"+str(pin)+"/direction", 'w') as f:
         f.write(dir)
         f.flush()
 
 def GPIOWrite(pin, value):
-    with open(GPIO_ROOT+"/gpio"+str(pin)+"/value", FMODE) as f:
+    with open(GPIO_ROOT+"/gpio"+str(pin)+"/value", 'w') as f:
         f.write(str(value))
         f.flush()
         print("Write", value, "to pin", pin)
